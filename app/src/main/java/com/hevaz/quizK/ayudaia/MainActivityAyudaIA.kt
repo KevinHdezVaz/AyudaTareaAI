47533a01a496193df9853813ee268f0d30e7a1e9
kevin hernandez <kevin.hernandez@caffenio.com>
2024-03-04 23:22:57 -0600

    bottom navigation

--- app/src/main/java/com/hevaz/quizK/ayudaia/MainActivityAyudaIA.kt
+++ app/src/main/java/com/hevaz/quizK/ayudaia/MainActivityAyudaIA.kt
@@ -1,12 +1,21 @@
 package com.hevaz.quizK.ayudaia
 
-import androidx.appcompat.app.AppCompatActivity
+import android.app.Dialog
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
-import android.view.Menu
-import android.widget.PopupMenu
+import android.view.Gravity
+import android.view.ViewGroup
+import android.view.Window
+import android.widget.ImageView
+import android.widget.LinearLayout
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
 import com.hevaz.quizK.R
+import com.hevaz.quizK.ayudaia.menuPrincipal.HomeFragment
 import com.hevaz.quizK.databinding.ActivityMainAyudaIaBinding
 
 
@@ -24,20 +33,53 @@ class MainActivityAyudaIA : AppCompatActivity() {
         setContentView(binding.root)
         val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
         navController = navHostFragment.navController
-        setupSmoothBottomMenu()
+
         observeDestinationChange()
 
+binding.fab.setOnClickListener {
+    showBottomDialog()
+}
+        binding.bottomNavigationView.background = null
+        val navController = NavHostFragment.findNavController(navHostFragment)
+        binding.bottomNavigationView.setupWithNavController(navController);
 
 
     }
 
-    private fun setupSmoothBottomMenu() {
-        val popupmenu = PopupMenu(this, null)
-        popupmenu.inflate(R.menu.menu_main)
-        val menu: Menu = popupmenu.getMenu()
-        binding.bottomBar.setupWithNavController(menu, navController)
-    }
 
+
+    private fun showBottomDialog() {
+        val dialog = Dialog(this)
+        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE)
+        dialog.setContentView(R.layout.bottomsheetlayout)
+        val videoLayout = dialog.findViewById<LinearLayout>(R.id.layoutVideo)
+        val shortsLayout = dialog.findViewById<LinearLayout>(R.id.layoutShorts)
+        val liveLayout = dialog.findViewById<LinearLayout>(R.id.layoutLive)
+        val cancelButton = dialog.findViewById<ImageView>(R.id.cancelButton)
+        videoLayout.setOnClickListener {
+            dialog.dismiss()
+            Toast.makeText(this@MainActivityAyudaIA, "Upload a Video is clicked", Toast.LENGTH_SHORT)
+                .show()
+        }
+        shortsLayout.setOnClickListener {
+            dialog.dismiss()
+            Toast.makeText(this@MainActivityAyudaIA, "Create a short is Clicked", Toast.LENGTH_SHORT)
+                .show()
+        }
+        liveLayout.setOnClickListener {
+            dialog.dismiss()
+            Toast.makeText(this@MainActivityAyudaIA, "Go live is Clicked", Toast.LENGTH_SHORT).show()
+        }
+        cancelButton.setOnClickListener { dialog.dismiss() }
+        dialog.show()
+        dialog.window!!.setLayout(
+            ViewGroup.LayoutParams.MATCH_PARENT,
+            ViewGroup.LayoutParams.WRAP_CONTENT
+        )
+        dialog.window!!.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+        dialog.window!!.attributes.windowAnimations = R.style.DialogAnimation
+        dialog.window!!.setGravity(Gravity.BOTTOM)
+    }
     private fun observeDestinationChange() {
         navController.addOnDestinationChangedListener { controller, destination, arguments ->
             when(destination.id) {
@@ -54,24 +96,20 @@ class MainActivityAyudaIA : AppCompatActivity() {
                 }
 */
                 R.id.introFragment -> {
-                    binding.bottomBar.hide()
+                    binding.bottomAppBar.hide()
                 }
 
                 else -> {
-                    binding.bottomBar.show()
+                     binding.bottomAppBar.show()
                 }
             }
         }
     }
-    fun hideSmoothBottomBar() {
-        // oculta el SmoothBottomBar
-        binding.bottomBar.hide()
-
+   fun hideSmoothBottomBar(){
+       binding.bottomAppBar.hide()
     }
+fun showSmooth(){
+    binding.bottomAppBar.show()
 
-    fun showSmooth() {
-        // oculta el SmoothBottomBar
-        binding.bottomBar.show()
-
-    }
+}
 }
--- app/src/main/java/com/hevaz/quizK/ayudaia/menuPrincipal/tomaFoto/Tab_Foto.kt
+++ app/src/main/java/com/hevaz/quizK/ayudaia/menuPrincipal/tomaFoto/Tab_Foto.kt
@@ -1,144 +1,81 @@
-package com.hevaz.quizK.ayudaia.menuPrincipal
+package com.hevaz.quizK.ayudaia.menuPrincipal.tomaFoto
 
-import android.annotation.SuppressLint
 import android.app.Dialog
-import android.content.ContentValues
 import android.content.Intent
-import android.content.res.Configuration
 import android.graphics.Color
 import android.graphics.drawable.ColorDrawable
 import android.net.Uri
 import android.os.Bundle
-import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
-import android.widget.TextView
 import androidx.activity.addCallback
 import androidx.appcompat.app.ActionBarDrawerToggle
-import androidx.appcompat.app.AppCompatDelegate
 import androidx.drawerlayout.widget.DrawerLayout
-import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
-import androidx.recyclerview.widget.LinearLayoutManager
-import com.google.firebase.auth.FirebaseAuth
 import com.hevaz.quizK.R
-import com.hevaz.quizK.activity.MainActivity
 import com.hevaz.quizK.ayudaia.MainActivityAyudaIA
-import com.hevaz.quizK.databinding.FragmentHomeBinding
+import com.hevaz.quizK.databinding.FragmentTabFotoBinding
 
+class Tab_Foto : Fragment() {
 
-class HomeFragment : Fragment(R.layout.fragment_home)   {
-    private lateinit var binding: FragmentHomeBinding
-     var epicDialog2: Dialog? = null
+    private lateinit var binding: FragmentTabFotoBinding // Reemplaza MyFragmentBinding con el nombre de tu clase de binding
+
+    var seguir: Button? = null
+    var epicDialog2: Dialog? = null
     var epicDialogTErminos: Dialog? = null
-     var seguir: Button? = null
-    @SuppressLint("NotifyDataSetChanged")
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
 
-        super.onViewCreated(view, savedInstanceState)
-        binding = FragmentHomeBinding.bind(view)
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
 
-        updateIcon()
-        binding.darktheme.setOnClickListener {
-            if (isDarkMode()) {
-                disableDarkMode()
-            } else {
-                enableDarkMode()
-            }
-            updateIcon()
-        }
+        binding = FragmentTabFotoBinding.inflate(inflater, container, false) // Reemplaza MyFragmentBinding con el nombre de tu clase de binding
 
 
-//para salir
         requireActivity().onBackPressedDispatcher.addCallback(viewLifecycleOwner) {
             requireActivity().finish()
         }
-
-
         hamburgesa()
 
 
 
 
-    }
 
-
-
-    private fun updateIcon() {
-        if (isDarkMode()) {
-            binding.darktheme.setImageResource(R.drawable.baseline_wb_sunny_24)
-        } else {
-            binding.darktheme.setImageResource(R.drawable.baseline_mode_night_24)
-        }
-    }
-    private fun enableDarkMode(){
-
-        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
-    }
-    public fun disableDarkMode(){
-        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+        return binding.root
 
     }
-
 
 
 
     private fun hamburgesa() {
 
-// Obtener referencia al NavigationView
-        val navigationView = binding.navigationView
-
-// Obtener referencia al TextView en el header
-        val headerView = navigationView.getHeaderView(0)
-        val txtUsuario = headerView.findViewById<TextView>(R.id.textUsuario)
-
-// Obtener la información del usuario actual de Firebase
-        val user = FirebaseAuth.getInstance().currentUser
-
-// Verificar si el usuario ha iniciado sesión y establecer el correo electrónico o el usuario en el TextView correspondiente
-        if (user != null) {
-            txtUsuario.text = user.email ?:  "Usuario"
-        } else {
-            txtUsuario.text = "Usuario"
-        }
 
         // Establecer un oyente de eventos para los elementos del menú
         binding.navigationView.setNavigationItemSelectedListener { menuItem ->
             // Manejar la selección del elemento del menú aquí
             when (menuItem.itemId) {
                 R.id.nav_acerca -> {
-               //     mostrarSalir()
+
                     binding.drawerLayout.closeDrawers()
 
                     true
                 }
 
                 R.id.navTerminos -> {
-                 //   terminos()
+
                     binding.drawerLayout.closeDrawers()
 
                     true
                 }
-                R.id.nav_night -> {
-                    if (isDarkMode()) {
-                        disableDarkMode()
-                    } else {
-                        enableDarkMode()
-                    }
-                    binding.drawerLayout.closeDrawers()
-                    true
-                }
+                /*
                 R.id.salir -> {
-                 //   FirebaseAuth.getInstance().signOut()
-                //    findNavController().navigate(R.id.action_homeFragment_to_loginFragment)
+                    FirebaseAuth.getInstance().signOut()
+                    findNavController().navigate(R.id.action_homeFragment_to_loginFragment)
 
                     true
                 }
 
-
+                 */
                 R.id.nav_share -> {
                     compartir()
                     true
@@ -168,26 +105,19 @@ class HomeFragment : Fragment(R.layout.fragment_home)   {
             override fun onDrawerStateChanged(newState: Int) {}
 
             override fun onDrawerClosed(drawerView: View) {
-                 (requireActivity() as MainActivityAyudaIA).showSmooth()
+                (requireActivity() as MainActivityAyudaIA).showSmooth()
             }
 
             override fun onDrawerOpened(drawerView: View) {
                 // Lógica a realizar cuando se abre el Navigation Drawer
-               (requireActivity() as MainActivityAyudaIA).hideSmoothBottomBar()
+                (requireActivity() as MainActivityAyudaIA).hideSmoothBottomBar()
             }
         })
 
 
 
     }
-    private fun isDarkMode(): Boolean {
-        val currentNightMode = resources.configuration.uiMode and Configuration.UI_MODE_NIGHT_MASK
-        return currentNightMode == Configuration.UI_MODE_NIGHT_YES
-    }
 
-    private fun modoDark() {
-        enableDarkMode()
-    }
 
 
 
@@ -210,9 +140,13 @@ class HomeFragment : Fragment(R.layout.fragment_home)   {
 
 
     }
+    override fun onResume() {
+        super.onResume()
+        binding.particleView.resume()
+    }
 
-
-
-
-
+    override fun onPause() {
+        super.onPause()
+        binding.particleView.pause()
+    }
 }
